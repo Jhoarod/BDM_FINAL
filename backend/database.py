95cd6b04c2cd6a2cd51e90e7c20779c43e333417
Jhoarod <jhoan.azul.7@gmail.com>
2025-11-18 22:42:38 -0500

    test

--- backend/database.py
+++ backend/database.py
@@ -5,7 +5,7 @@ import os
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
-    raise ValueError("❌ No existe la variable de entorno DATABASE_URL.")
+    raise ValueError(" No existe la variable de entorno DATABASE_URL.")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
